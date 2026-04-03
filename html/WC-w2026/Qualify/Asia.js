40e09c17cdcc84b53eff1b3c09443504a1b92481
Agron Kovaçi <covaci61g@gmail.com>
2026-04-04 00:57:04 +0300

    Add files via upload

---
+++ html/WC-w2026/Qualify/Asia.js
@@ -0,0 +1,16 @@
+
+// All tournament participants (no fixed team in round-robin)
+const data = [
+    [1, "Tcheran 11.0 x64(AVX2-5Pct) [WILDCARD]", 153.0, 176, 2619.5, +19.5, 86.9],
+    [2, "Altair 7.0.0 x64(AVX2-5Pct)", 140.0, 176, 2655.1, -3.9, 79.5],
+    [3, "Turbulence 0.0.8 x64(AVX2-5Pct)", 135.0, 176, 2626.1, -2.9, 76.7],
+    [4, "Elixir 3.0 x64(BMI2-5Pct)", 127.0, 176, 2594.6, -9.3, 72.1],
+    [5, "Rice 8.0 x64(AVX2-5Pct)", 124.0, 176, 2580.8, -10.1, 70.4],
+    [6, "RukChess 4.2.0 x64(5Pct) [WILDCARD]", 92.0, 176, 2222.1, +22.1, 52.2],
+    [7, "Avalanche 2.1.0 x64(AVX2-5Pct)", 88.5, 176, 2373.3, -8.7, 50.2],
+    [8, "Myrddin 0.94 x64(5Pct) [WILDCARD]", 58.0, 176, 2121.5, +4.5, 32.9],
+    [9, "Bit-Genie 9.0 x64(BMI2-5Pct)", 44.5, 176, 2095.8, -6.2, 25.2],
+    [10, "Potential 3.0.0 x64(Haswell-5Pct) [WILDCARD]", 44.0, 176, 2093.3, -6.6, 25.0],
+    [11, "Vengeance 3.1.0 x64(BMI2-5Pct)", 38.0, 176, 2007.1, +1.2, 21.5],
+    [12, "Mess 0.3.0 x64(5Pct)", 12.0, 176, 1806.0, -2.0, 6.8]
+];
---
+++ html/WC-w2026/Qualify/Turkey.js
@@ -0,0 +1,14 @@
+
+// All tournament participants (no fixed team in round-robin)
+const data = [
+    [1, "Astra 6.1.1 x64(BMI2-5Pct)", 167.0, 192, 2766.3, +13.4, 86.9],
+    [2, "Astra 5.2.0 x64(BMI2-5Pct)", 145.0, 192, 2684.8, -9.1, 75.5],
+    [3, "Black Marlin 9.0 x64(AVX2-5Pct)", 138.0, 192, 2624.6, +15.6, 71.8],
+    [4, "Devre 6.0 x64(AVX2-5Pct)", 132.5, 192, 2655.8, -8.1, 69.0],
+    [5, "Black Marlin 8.0 x64(AVX2-5Pct)", 130.5, 192, 2599.1, +9.1, 67.9],
+    [6, "Devre 5.0 x64(AVX2-5Pct)", 125.0, 192, 2586.5, -1.5, 65.1],
+    [7, "Defenchess 2.2 x64(5Pct)", 53.5, 192, 2295.8, -4.1, 27.8],
+    [8, "Defenchess 2.3 x64(5Pct)", 51.0, 192, 2292.6, -7.3, 26.5],
+    [9, "Potential 3.0.0 x64(Haswell-5Pct)", 11.0, 192, 2092.0, -8.0, 5.7],
+    [10, "Potential 2.0.0 x64(Haswell-5Pct)", 6.5, 192, 1963.6, -2.3, 3.3]
+];
